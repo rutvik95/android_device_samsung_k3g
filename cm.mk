63515102853aafbeb4f79409d30ec021f3ecb4cc
Rutvik Rajagopal <rutvik_95@yahoo.co.in>
2015-03-13 19:04:51 +0530

    mkvendor.sh: Initial commit of k3g

---
+++ cm.mk
@@ -0,0 +1,18 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := k3g
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/k3g/device_k3g.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := k3g
+PRODUCT_NAME := cm_k3g
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := k3g
+PRODUCT_MANUFACTURER := samsung
